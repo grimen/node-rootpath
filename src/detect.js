eed92453d44b6de20eb4625eaba0b08f7c346f0f
Jonas Grimfelt <grimen@gmail.com>
2019-12-27 21:55:14 -0500

    compat: Node 8

--- src/detect.js
+++ src/detect.js
@@ -4,7 +4,7 @@
 -------------------------------------- */
 
 const path = require('path')
-const fs = require('fs').promises
+const fs = require('fs-extra')
 
 
 /* =========================================
